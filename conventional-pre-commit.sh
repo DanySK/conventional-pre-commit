89f0392c6152dc01b599d3dc2ae90d5c44e8bf9e
Danilo Pianini <danilo.pianini@unibo.it>
2021-11-06 23:07:54 +0100

    Try colors

--- conventional-pre-commit.sh
+++ conventional-pre-commit.sh
@@ -34,25 +34,31 @@ if grep -Eq "$pattern" "$msg_file"; then
     exit 0
 fi
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
 echo "[Commit message] $( cat "$msg_file" )"
 echo "
-Your commit message does not follow Conventional Commits formatting
-https://www.conventionalcommits.org/
+Your commit message does ${RED}not${NC} follow ${PURPLE}Conventional Commits${NC} formatting
+${BLUE}https://www.conventionalcommits.org/${NC}
 
 Conventional Commits start with one of the below types:
-    $(IFS=' '; echo "${types[*]}")
-followed by an optional scope within parentheses,
-followed by an exclamation mark (!) in case of breaking change,
+    ${GREEN}$(IFS=' '; echo "${types[*]}")${NC}
+followed by an ${PURPLE}optional scope within parentheses${NC},
+followed by an ${RED}exclamation mark (!) in case of breaking change${NC},
 followed by a colon (:),
 followed by the commit message.
 
 Example commit message fixing a bug non-breaking backwards compatibility:
-    fix(module): fix bug #42
+    ${GREEN}fix(module): fix bug #42${NC}
 
 Example commit message adding a non-breaking feature:
-    feat(module): add new API
+    ${GREEN}feat(module): add new API${NC}
 
 Example commit message with a breaking change:
-    refactor(module)!: remove infinite loop
+    ${GREEN}refactor(module)!: remove infinite loop${NC}
 "
 exit 1
